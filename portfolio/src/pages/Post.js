768800d2847865f0ce619f048532dc80de6efd3d
axeliono <69375446+axeliono@users.noreply.github.com>
2021-04-04 20:57:05 -0500

    initialized sanity.io files

--- portfolio/src/pages/Post.js
+++ portfolio/src/pages/Post.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React,{ useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 
 const Post = () => {
+    const state = useSelector((state) => state);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        
+    })
     return (
         <div>
             
